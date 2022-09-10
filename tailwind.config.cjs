985f146c4610f53db179a745b85a43d4ff615dbe
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2022-09-10 13:09:42 +0530

    add logo and teaser

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,25 @@
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'red-brand': '#DB1F26',
+        'yellow-brand': '#F6841F',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addUtilities }) => {
+      addUtilities({
+        '.flex-center': {
+          display: 'flex',
+          'justify-content': 'center',
+          'align-items': 'center',
+        },
+      })
+    }),
+  ],
 }
